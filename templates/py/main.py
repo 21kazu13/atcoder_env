3d52842d36486ed7fd4a4462c76ae8928e7864f8
入倉和志 <kazu@kazu-mac.local>
2022-03-05 09:14:07 +0900

    modify templeate

--- templates/py/main.py
+++ templates/py/main.py
@@ -2,12 +2,12 @@
 import sys
 from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right  # type: ignore
 from collections import Counter, defaultdict, deque  # type: ignore
-from fractions import gcd  # type: ignore
+from math import gcd  # type: ignore
 from heapq import heapify, heappop, heappush, heappushpop, heapreplace, merge  # type: ignore
 from itertools import accumulate, combinations, permutations, product  # type: ignore
 
-
 def LI(): return list(map(int, sys.stdin.buffer.readline().split()))
+def MI(): return map(int, sys.stdin.buffer.readline().split())
 def I(): return int(sys.stdin.buffer.readline())
 def LS(): return sys.stdin.buffer.readline().rstrip().decode("utf-8").split()
 def S(): return sys.stdin.buffer.readline().rstrip().decode("utf-8")
