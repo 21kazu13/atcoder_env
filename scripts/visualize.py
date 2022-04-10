cb6caade7774d974416bf6c2fe4a708ff8f16a49
Kazushi Irikura <0107kazu@gmail.com>
2022-04-10 16:42:53 +0900

    Change vscode snippets and graph visualizer - Mount directly from host - Define VisualGraph class in graph visualizer - remove pycache from git management

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -8,8 +8,6 @@
 ## https://graphviz.org/doc/info/lang.html
 ## https://ja.wikipedia.org/wiki/DOT%E8%A8%80%E8%AA%9E
 
-## できればクラス化したいな...
-## http://pineplanter.moo.jp/non-it-salaryman/2018/09/04/python-external-class/
 '''
 Required format for directed graph by markdown preview enhanced is here:
 * for undirected graph, place 'graph' and '--' instead of 'digraph' and '->'
@@ -22,44 +20,63 @@ digraph G {
 ```
 
 '''
-
-def outDirectedGraph(G: list,s='graph'):
+class VisualGraph:
     '''
-    input: Directed Graph showed in adjacency list
-    output: text formatted to markdown preview enhanced
+    - Check basic contents of graph structure
+    - Generate markdown file for visualization
     '''
-    result = ['```dot','digraph G {']
-    for num,item in enumerate(G):
-        if len(item)==0:
-            result.append(f'\t{num};')
-            continue
-        for node in item:
-            result.append(f'\t{num} -> {node};')
-    result.append('}')
-    result.append('```')
+    def __init__(self, G: list, hasdirection=False, weightcolumn=0) -> None:
+        self.neighborList = G
+        self.numberOfNodes = len(G)
+        # 0: no cost of edges, 1: has cost of edges: 
+        self.hasWeight = 0 if type(G[0]) != int else 1
+        self.hasDirection = 0 if not hasdirection else 1
+        self.weightColumn = weightcolumn
+        self.numberOfEdges = sum([len(G[i]) for i in range(self.numberOfNodes)])
+        self.expressionOfEdges = '->'
+        if not self.hasDirection:
+            self.numberOfEdges //= 2
+            self.expressionOfEdges = '--'
+    
+    def __str__(self) -> str:
+        type = 'directed' if self.hasDirection else 'undirected'
+        res = f'This is {type} graph.\n'
+        res += f'  Number of Nodes: {self.numberOfNodes}\n'
+        res += f'  Number of Edges: {self.numberOfEdges}\n'
+        return res
 
-    with open(s+'.md','w') as f:
-        f.write('\n'.join(result))
+    def _generateHeadFoot(self) -> tuple:
+        headers = ['```dot','graph G{']
+        if self.hasDirection:
+            headers[1] = 'digraph G{'
+        footers = ['}','```']
+        return (headers,footers)
 
-    return
+    def _extractDestCost(self, org: int, node) -> tuple:
+        if self.hasWeight:
+            cost = node[self.weightColumn]
+            dest = node[self.weightColumn^1]
+            ex = f'\t{org} {self.expressionOfEdges} {dest} [label="{cost}"];'
+        else:
+            dest = node
+            cost = ''
+            ex = f'\t{org} {self.expressionOfEdges} {dest};'
+        return(dest,ex)
 
-def outUndirectedGraph(G: list,s='graph'):
-    '''
-    input: Undirected Graph showed in adjacency list
-    output: text formatted to markdown preview enhanced
-    '''
-    result = ['```dot','graph G {']
-    for num,item in enumerate(G):
-        if len(item)==0:
-            result.append(f'\t{num};')
-            continue
-        for node in item:
-            if num <= node:
-                result.append(f'\t{num} -- {node};')
-    result.append('}')
-    result.append('```')
 
-    with open(s+'.md','w') as f:
-        f.write('\n'.join(result))
+    def generateMarkdown(self,filename='graph') -> None:
+        headers, footers = self._generateHeadFoot()
+        res = []
+        for org,item in enumerate(self.neighborList):
+            if len(item)==0:
+                res.append(f'\t{org};')
+                continue
+            for node in item:
+                dest, expression = self._extractDestCost(org,node)
+                if org > dest and not self.hasDirection:
+                    continue
+                res.append(expression)
 
-    return
+                with open(filename+'.md','w') as f:
+                    f.write('\n'.join(headers+res+footers))
+        return
