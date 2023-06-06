25c26d859de476811c2b757c6057484141b0c172
Kazushi Irikura <0107kazu@gmail.com>
2023-06-06 13:09:37 +0900

    Modify function for copying template

--- scripts/copytemplate.sh
+++ scripts/copytemplate.sh
@@ -1,12 +1,14 @@
 cptlt() {
     local fn
     if [ $# = 0 ]; then
-        echo "[ERROR] Specify file name"
-        return 1
-    elif [ -e $1 ]; then
+        fn=$(date +%Y%m%d).py
+    else
+        fn=$1
+    fi
+    if [ -e $fn ]; then
         echo "[ERROR] File already exist"
         return 1
     fi
-    cp ~/.config/atcoder-cli-nodejs/py/main.py $1 &&  code $1
+    cp ~/.config/atcoder-cli-nodejs/py/main.py $fn && code $fn
     return $?
 }
